9c9f2c7602e93b8afdc5090fa220bd371667048e
dovgreenwood <dov.greenwood@gmail.com>
2021-06-17 16:01:33 -0400

    began new prefix detaching methods

--- utils/deconstruct2.py
+++ utils/deconstruct2.py
@@ -159,5 +159,123 @@ def aramaic_noun_root(token):
     return list(unique) if unique else []
 
 
-def identify_prefix(token):
-    return
+def identify_prefix(token, lang='all'):
+    """
+    Identifies whether a particular token MAY have a prefix. This does not confirm whether the token
+    DOES have a prefix--only if the first consonant+vowel CONSTITUTE a possible prefix. So, for example,
+    it will identify anything beginning with "קָ־" as having a possible prefix, even if it is an actual word,
+    such as "קָאזָא."
+
+    :param token: the token to be checked, a string
+    :param lang: the language whose prefixes are to be checked, a string: 'heb,' 'aram,' 'all' (default)
+    :return: True if there MAY be an attached prefix, False otherwise
+
+    TODO: Merge with remove_prefix, add further rules for prefixes--
+    https://en.wikipedia.org/wiki/Prefixes_in_Hebrew
+    https://hebrew-academy.org.il/2013/07/18/%D7%A0%D7%99%D7%A7%D7%95%D7%93-%D7%90%D7%95%D7%AA%D7%99%D7%95%D7%AA-%D7%94%D7%A9%D7%99%D7%9E%D7%95%D7%A9/
+    https://github.com/Dicta-Israel-Center-for-Text-Analysis/Talmud-Bavli-with-Nikud
+    """
+
+    aram_prefixes = ['הֲ', 'דִּ', 'דְּ', 'אַ', 'קָ']
+    heb_prefixes = ['שֶׁ']
+    both_prefixes = ['מִ', 'מִי', 'מֵ', 'מֵי',
+                     'לְ', 'לָ', 'לַ', 'לִ', 'לֵ',
+                     'כֵ', 'כָ', 'כְ', 'כִ', 'כִי',
+                     'כֵּ', 'כָּ', 'כְּ', 'כִּ', 'כִּי',
+                     'וְ', 'וּ',
+                     'הֶָ', 'הָ', 'הַ',
+                     'בֵ', 'בִ', 'בַ', 'בְ',
+                     'בֵּ', 'בִּ', 'בָּ', 'בְּ']
+
+    for p in both_prefixes:
+        if token[:len(p)] == p:
+            return True
+    if lang == 'aram' or lang == 'all':
+        for p in aram_prefixes:
+            if token[:len(p)] == p:
+                return True
+    if lang == 'heb' or lang == 'all':
+        for p in heb_prefixes:
+            if token[:len(p)] == p:
+                return True
+
+    return False
+
+
+def detach_prefix(token, lang='all', char=1):
+    """
+    BETA: Improved version of identify_prefix, see "TODO."
+
+    Given a token, identifies possible prefix(es) and returns a list of possible words corresponding to the token.
+    These words are not checked first to see if they "exist"; they only need to POSSIBLY exist, given the rules of
+    prefixes.
+
+    :param token: the token to be checked, a string
+    :param lang: the language whose prefixes are to be checked, a string: 'heb,' 'aram,' 'all' (default)
+    :param char: DO NOT CHANGE. In the case of multiple possible prefixes, this tracks their order.
+    :return: a list of strings, which are the possible corresponding words
+
+    TODO: After deleting identify_prefix, make variables static.
+
+    ALGORITHM:
+    1) identify, detach, and index all possible prefixes
+        a. moving from right to left, individually identify if the letter-vowel combination is a possible prefix
+        b. if it is a possible prefix, remove it from the word, put it as the START** of an array called "prefs,"
+           and continue
+        c. once a letter-vowel combination is reached that cannot be a prefix, save it as "nex" and stop (do not remove)
+        d. save the remaining characters of the word into an array called "roots" and as a variable called word
+    2) working FORWARD through prefs, identify if the prefix is possible based on its conjugation rules
+        a. pop first item in prefs and save it as "curr"
+        b. identify whether it is a valid prefix based on its own rules, with the following information:
+            i. the prefix itself
+            ii. the next vowel-letter combination in the word, which was saved in "nex"
+            iii. the index of the current prefix = len(prefs) [if there is a ו or ה, the index must be 0]
+    3) Append curr to the front of word, set nex to curr.
+    4) If (2) identified curr as possible, append word to roots/
+    5) Repeat from (2) until len(prefs) == 0.
+    6) Return roots.
+
+    ** This is required so that the started/end rules for ו or ה can both be identified as index = 0.
+    """
+
+    possibilities = [token]
+
+    aram_only = ['הֲ', 'דִּ', 'דְּ', 'אַ', 'קָ']
+    heb_only = ['שֶׁ']
+    # two letter and tzere prefixes currently commented since I don't know if they really appear (see Wiki)
+    conjunction = ['וְ', 'וּ']
+    inseparable_prepositions = ['לְ', 'לָ', 'לַ', 'לִ',  # 'לֵ',
+                                'כָ', 'כְ', 'כִ',  # 'כִי' ,'כֵ',
+                                'כָּ', 'כְּ', 'כִּ',  # 'כֵּ', 'כִּי',
+                                'בִ', 'בַ', 'בְ',  # 'בֵ',
+                                'בִּ', 'בָּ', 'בְּ']  # 'בֵּ',
+    mem_preposition = ['מִ', 'מֵ']  # 'מִי', 'מֵי',
+    definite_article = ['הֶָ', 'הָ', 'הַ']
+
+    pref2 = False
+
+    if char == 1:
+        for p in conjunction:
+            if token[:len(p)] == p:
+                possibilities.append(token[len(p):])
+                pref2 = True
+    for p in inseparable_prepositions:
+        pass
+    for p in mem_preposition:
+        pass
+    for p in definite_article:
+        pass
+
+    if lang == 'aram' or lang == 'all':
+        for p in aram_only:
+            if token[:len(p)] == p:
+                return True
+    if lang == 'heb' or lang == 'all':
+        for p in heb_only:
+            if token[:len(p)] == p:
+                return True
+
+    possibilities += detach_prefix(possibilities[1], lang, char+1) if pref2 else []
+
+    return possibilities
+
